3cd3063e26791639ce0f43156bdf1944fef4f5d8
tsuji-tomonori <160441107@ccalumni.meijo-u.ac.jp>
2022-07-03 20:05:37 +0900

    pathがだめだったので

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import aws_cdk as cdk
-from aws_cdk import Aspects, Tags
+from aws_cdk import Aspects, Names, Tags
 import jsii
 
 from my_cdk_sample.my_cdk_sample_stack import MyCdkSampleStack
@@ -10,7 +10,7 @@ from my_cdk_sample.my_cdk_sample_stack import MyCdkSampleStack
 @jsii.implements(cdk.IAspect)
 class ResourceTags:
     def visit(self, node):
-        Tags.of(node).add("resource", node.path)
+        Tags.of(node).add("resource", Names.unique_id(node))
 
 
 app = cdk.App()

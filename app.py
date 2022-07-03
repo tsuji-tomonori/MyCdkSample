7a2f510b7f3a1779b2ab2ce9e83e2daa674b6c2f
tsuji-tomonori <160441107@ccalumni.meijo-u.ac.jp>
2022-07-03 17:32:27 +0900

    リソースタグを自動でつけれるようにお試し

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
-import os
 
 import aws_cdk as cdk
-from aws_cdk import Tags
+from aws_cdk import Aspects, Tags
+import jsii
 
 from my_cdk_sample.my_cdk_sample_stack import MyCdkSampleStack
+
+
+@jsii.implements(cdk.IAspect)
+class ResourceTags:
+    def visit(self, node):
+        Tags.of(node).add("resource", node.id)
 
 
 app = cdk.App()
 stack = MyCdkSampleStack(app, "MyCdkSampleStack")
 Tags.of(stack).add("service", "cdk_sample")
+Aspects.of(stack).add(ResourceTags())
 app.synth()
